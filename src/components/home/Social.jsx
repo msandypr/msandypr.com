c3a3359463b7c27edb5cee43feac8e9dadd66897
msandypr <msandypr@gmail.com>
2023-01-15 18:18:19 +0700

    Adding Header and Home Components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://instagram.com/msandypr" className="home__social-icon" target="blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://github.com/msandypr" className="home__social-icon" target="blank">
+        <i class="uil uil-github"></i>
+        </a>
+        <a href="https://linkedin.com/in/msandypr" className="home__social-icon" target="blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
