52d9ac91c80a1c7ebbc8ee2cafb94a174e22e485
msandypr <msandypr.dev@gmail.com>
2024-09-24 14:14:21 +0700

    fix(components): hero section responsive

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,3 @@
-import memojiImage from "@/assets/images/memoji-computer.png";
 import sandyHero from "@/assets/images/sandy-rounded-hero.png"
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg"
@@ -6,7 +5,7 @@ import { memo } from "react";
 
 export const HeroSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-32 md:py-48 lg:py-60">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={sandyHero} className="size-[100px] border border-gray-800 rounded-full" alt="MSANDYPR"></Image>
@@ -15,9 +14,11 @@ export const HeroSection = () => {
             <div className="text-sm font-medium">Available for Projects!</div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">Software Engineer</h1>
-        <p className="text-center mt-4 text-white/60">I am a Software Engineer with expertise in JavaScript and PHP, proficient in various frameworks such as React.js, Node.js, CodeIgniter, Laravel, TailwindCSS, and Bootstrap.</p>
-        <div className="flex flex-col items-center mt-8">
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Muhammad Sandy Putra Riyadi</h1>
+          <p className="text-center mt-4 text-white/60 md:text-lg">I am a Software Engineer with expertise in JavaScript and PHP, proficient in various frameworks such as React.js, Node.js, CodeIgniter, Laravel, TailwindCSS, and Bootstrap.</p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8">
           <button className="inline-flex items-center gap-2 broder border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore my Work!</span>
             <ArrowDown className="size-4" />
