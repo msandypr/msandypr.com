8bd0acf83ea8cffe99a37164f5ce2183078d9973
msandypr <msandypr@protonmail.com>
2024-10-01 14:57:14 +0700

    feat(components): styling hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,29 +1,77 @@
-import sandyHero from "@/assets/images/sandy-rounded-hero.png"
+import sandyHero from "@/assets/images/sandy-rounded-hero.png";
 import Image from "next/image";
-import ArrowDown from "@/assets/icons/arrow-down.svg"
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
+import { HeroOrbit } from "../components/HeroOrbit";
+import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0">
-      <div className="absolute inset-0 -z-30 opacity-5" style={{
-        backgroundImage: `url(${grainImage.src})`,
-      }}></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-50 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-42}>
+          <div className="size-2 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-2 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+      </div>
       <div className="container">
         <div className="flex flex-col items-center">
-          <Image src={sandyHero} className="size-[100px] border border-gray-800 rounded-full" alt="MSANDYPR"></Image>
+          <Image
+            src={sandyHero}
+            className="size-[100px] border border-gray-800 rounded-full"
+            alt="MSANDYPR"
+          ></Image>
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
             <div className="text-sm font-medium">Available for Projects!</div>
           </div>
         </div>
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Muhammad Sandy Putra Riyadi</h1>
-          <p className="text-center mt-4 text-white/60 md:text-lg">I am a Software Engineer with expertise in JavaScript and PHP, proficient in various frameworks such as React.js, Node.js, CodeIgniter, Laravel, TailwindCSS, and Bootstrap.</p>
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Muhammad Sandy Putra Riyadi
+          </h1>
+          <p className="text-center mt-4 text-white/60 md:text-lg">
+            I am a Software Engineer with expertise in JavaScript and PHP,
+            proficient in various frameworks such as React.js, Node.js,
+            CodeIgniter, Laravel, TailwindCSS, and Bootstrap.
+          </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8">
           <button className="inline-flex items-center gap-2 broder border-white/15 px-6 h-12 rounded-xl">
